d16e45c4dc9bd1230939e62698a8dbe277c63fd8
Jun <jun@jun.local>
2026-01-20 15:28:40 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::io::{self, Write};
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     print!("$ ");
     io::stdout().flush().unwrap();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+    input = input.trim().to_string();
+    println!("{}: command not found", input);
+    Ok(())
 }
