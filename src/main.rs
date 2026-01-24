b02ce098e4b44a6ace2493ed065f8ebf691a1620
Jun <125691637+aujmn@users.noreply.github.com>
2026-01-24 12:29:37 +0800

    Quoting: Double quotes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 mod builtin;
 mod env;
 
-use builtin::{TypeResult::*, cd_handler, type_handler};
 use std::{
     io::{self, Write},
     path::PathBuf,
 };
+
+use crate::builtin::{TypeResult::*, cd_handler, type_handler};
 
 fn main() -> io::Result<()> {
     let mut input = String::with_capacity(4096);
@@ -107,8 +108,6 @@ impl<'a> From<io::Error> for SwitcherError<'a> {
     }
 }
 
-// impl<'a> std::error::Error for SwitcherError<'a> {}
-
 impl<'a> std::fmt::Display for SwitcherError<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
@@ -130,6 +129,7 @@ fn parser(input: &str) -> io::Result<Vec<String>> {
     let mut args = vec![];
     let mut arg = String::with_capacity(64);
     let mut between_single_quotes = false;
+    let mut between_double_quotes = false;
     for c in input.chars()
     // `bytes()` or `chars()`?
     {
@@ -139,19 +139,32 @@ fn parser(input: &str) -> io::Result<Vec<String>> {
             } else {
                 arg.push(c);
             }
+        } else if between_double_quotes {
+            if c == '\"' {
+                between_double_quotes = false;
+            } else {
+                arg.push(c);
+            }
         } else if c.is_whitespace() {
+            // the only time a word is finished (the only time to push `arg`
+            // into `args`) is when it's outside quote scoping and it's
+            // followed by whitespace.
             if !arg.is_empty() {
                 args.push(arg.clone());
                 arg.clear();
             }
         } else if c == '\'' {
             between_single_quotes = true;
+        } else if c == '"' {
+            between_double_quotes = true;
         } else {
             arg.push(c);
         }
     }
     if between_single_quotes {
         Err(io::Error::other("Input contains dangling single quotes"))
+    } else if between_double_quotes {
+        Err(io::Error::other("Input contains dangling double quotes"))
     } else {
         if !arg.is_empty() {
             args.push(arg);
@@ -171,7 +184,29 @@ mod test {
     #[case("'hello''world'", vec!["helloworld"])]
     #[case("hello''world", vec!["helloworld"])]
     #[case("'' '''' ''", vec![])]
-    fn test_parser(#[case] input: &str, #[case] expected: Vec<&str>) {
+    fn test_parser_single_quotes(#[case] input: &str, #[case] expected: Vec<&str>) {
+        assert!(parser(&String::from(input)).is_ok());
+        assert_eq!(parser(&String::from(input)).unwrap(), expected);
+    }
+
+    #[rstest]
+    #[case("\"hello    world\"", vec!["hello    world"])]
+    #[case("\"hello\"\"world\"", vec!["helloworld"])]
+    #[case("\"hello\" \"world\"", vec!["hello", "world"])]
+    #[case("\"shell's test\"", vec!["shell's test"])]
+    #[case("\"\" \"\"\"\" \"\"", vec![])]
+    fn test_parser_double_quotes(#[case] input: &str, #[case] expected: Vec<&str>) {
+        assert!(parser(&String::from(input)).is_ok());
+        assert_eq!(parser(&String::from(input)).unwrap(), expected);
+    }
+
+    #[rstest]
+    #[case("'\"'", vec!["\""])]
+    #[case("\"'\"", vec!["'"])]
+    #[case("'\"a'\"b\"", vec!["\"ab"])]
+    #[case("\"'a\"'b'", vec!["'ab"])]
+    #[case("\"a\"'b'", vec!["ab"])]
+    fn test_parser_mixed_quotes(#[case] input: &str, #[case] expected: Vec<&str>) {
         assert!(parser(&String::from(input)).is_ok());
         assert_eq!(parser(&String::from(input)).unwrap(), expected);
     }
